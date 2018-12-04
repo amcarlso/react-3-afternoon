80c62ccd514061a3ea6fa8ee0e73d1d384318c0b
Allen Carlson <allen.carlson123@gmail.com>
2018-12-04 16:37:27 -0700

    Completed steps 1-6, added searchPost to Search.js

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -7,6 +7,15 @@ import SearchIcon from 'react-icons/lib/md/search';
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  constructor(){
+    super()
+
+  }
+
+  searchPost(){
+    
+  }
+
   render() {
     return (
       <section className="Search__parent">
